52e72b022247936473d16f595ed3b10098dfd1d8
Hkiee <KersiQQ@gmail.com>
2026-01-08 01:12:44 +0300

    minor fixes into default_handlers

--- src/entrypoints/__init__.py
+++ src/entrypoints/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from src.entrypoints.default_handlers.health_check import router as health_router
+from src.entrypoints.default_handlers import router as health_router
 
 router = APIRouter()
 router.include_router(health_router)
